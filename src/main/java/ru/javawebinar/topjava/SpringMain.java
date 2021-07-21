3559d1b8caea1f1db000bec28b386ee054c97ab6
Avgur33 <avgur33@gmail.com>
2021-07-21 15:58:26 +0300

    repair somebugs

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.util.Arrays;
@@ -9,32 +8,19 @@ public class SpringMain {
     public static void main(String[] args) {
         // java 7 automatic resource management (ARM)
 
-        /*try(GenericXmlApplicationContext appCtx = new GenericXmlApplicationContext()){
+
+        try(GenericXmlApplicationContext appCtx = new GenericXmlApplicationContext()){
             appCtx.getEnvironment().setActiveProfiles(Profiles.POSTGRES_DB,Profiles.DATAJPA);
             appCtx.load("spring/spring-app.xml","spring/spring-db.xml");
             appCtx.refresh();
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
         }
-*/
-        try(ClassPathXmlApplicationContext appCtx = new ClassPathXmlApplicationContext(new String[]{"spring/spring-app.xml","spring/spring-db.xml"}, false) ){
+
+   /*     try(ClassPathXmlApplicationContext appCtx = new ClassPathXmlApplicationContext(new String[]{"spring/spring-app.xml","spring/spring-db.xml"}, false) ){
             appCtx.getEnvironment().setActiveProfiles(Profiles.POSTGRES_DB,Profiles.JPA);
             appCtx.refresh();
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
         }
-
-
-        /*ConfigurableApplicationContext appCtx = null;
-        try  {
-            appCtx = new ClassPathXmlApplicationContext(new String[]{"spring/spring-app.xml","spring/spring-db.xml"}, false);
-            appCtx.getEnvironment().setActiveProfiles(Profiles.HSQL_DB,Profiles.JDBC);
-            appCtx.refresh();
-            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
-        }finally {
-            if (appCtx != null){
-                appCtx.close();
-            }
-        }*/
+*/
     }
 }
